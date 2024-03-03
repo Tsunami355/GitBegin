2d21053bad02114057155f6bb4b582dd75b0311c
Tsunami355 <136748723+Tsunami355@users.noreply.github.com>
2024-03-03 23:05:18 +0200

    Create calculator

---
+++ app.py
@@ -0,0 +1,16 @@
+import argparse
+parser = argparse.ArgumentParser(description='Calculator')
+
+parser.add_argument('num1', type=float, help='First number')
+parser.add_argument('num2', type=float, help='Second number')
+parser.add_argument('op', choices=["+", "-"], help='Operation')
+
+args = parser.parse_args()
+
+if args.op == "+":
+    result = args.num1 + args.num2
+elif args.op == "-":
+    result = args.num1 - args.num2
+
+
+print(f'{args.num1} {args.op} {args.num2} = {result}')
