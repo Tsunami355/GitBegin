861efc49e9b40f9765de9fd8ae66798f4442c18d
Tsunami355 <136748723+Tsunami355@users.noreply.github.com>
2024-03-04 12:15:38 +0200

    develop division

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ parser = argparse.ArgumentParser(description='Calculator')
 
 parser.add_argument('num1', type=float, help='First number')
 parser.add_argument('num2', type=float, help='Second number')
-parser.add_argument('op', choices=["+", "-"], help='Operation')
+parser.add_argument('op', choices=["+", "-", "/"], help='Operation')
 
 args = parser.parse_args()
 
@@ -11,6 +11,12 @@ if args.op == "+":
     result = args.num1 + args.num2
 elif args.op == "-":
     result = args.num1 - args.num2
+elif args.op == "/":
+    if args.num2 != 0:
+        result = args.num1 / args.num2
+    else:
+        print("Error: Division by zero is not allowed.")
+        exit(1)
 
 
 print(f'{args.num1} {args.op} {args.num2} = {result}')
